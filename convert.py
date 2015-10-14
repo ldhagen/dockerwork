537fc7b6ed7837f741b02680a8d070b90a30e115
ldhagen <ldhagen@saconsumer.com>
2015-10-14 09:48:06 -0500

    made convert effecient

--- convert.py
+++ convert.py
@@ -2,18 +2,20 @@
 
 import openpyxl as ox, sqlite3 as sl, string
 
+def iter_rows(ws):
+    for row in ws.iter_rows():
+        yield [cell.value for cell in row]
+
 wb = ox.load_workbook(filename='High_Value_Data_Sets.xlsx', use_iterators=True)
 ws = wb.get_active_sheet()
 
 conn = sl.connect('TexScript.db')
+c1 = conn.cursor()
+c1.execute('create table inmates ("SID Number" ,"TDCJ Number" ,"Name" ,"Current Facility" ,"Gender" ,"Race" ,"DOB" ,"Projected Release" ,"Maximum Sentence Date" ,"Parole Eligibility Date" ,"Case Number" ,"County" ,"Offense Code" ,"Offense" ,"Sentence Date" ,"Offense Date" ,"Sentence Years" ,"Last Parole Decision" ,"Next Parole Review Date" ,"Parole Review Status")')
 
-conn.execute('create table inmates ("SID Number" ,"TDCJ Number" ,"Name" ,"Current Facility" ,"Gender" ,"Race" ,"DOB" ,"Projected Release" ,"Maximum Sentence Date" ,"Parole Eligibility Date" ,"Case Number" ,"County" ,"Offense Code" ,"Offense" ,"Sentence Date" ,"Offense Date" ,"Sentence Years" ,"Last Parole Decision" ,"Next Parole Review Date" ,"Parole Review Status")')
-
-for x in range(2,ws.max_row):
-    current_list = []
-    for y in string.ascii_uppercase[:20]:
-        current_list.append(ws[y + str(x)].value)
-    conn.execute('insert into inmates ("SID Number" ,"TDCJ Number" ,"Name" ,"Current Facility" ,"Gender" ,"Race" ,"DOB" ,"Projected Release" ,"Maximum Sentence Date" ,"Parole Eligibility Date" ,"Case Number" ,"County" ,"Offense Code" ,"Offense" ,"Sentence Date" ,"Offense Date" ,"Sentence Years" ,"Last Parole Decision" ,"Next Parole Review Date" ,"Parole Review Status") values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )', current_list)
-    conn.commit()
-    print x
-
+count = 0
+for x in iter_rows(ws):
+    c1.execute('insert into inmates ("SID Number" ,"TDCJ Number" ,"Name" ,"Current Facility" ,"Gender" ,"Race" ,"DOB" ,"Projected Release" ,"Maximum Sentence Date" ,"Parole Eligibility Date" ,"Case Number" ,"County" ,"Offense Code" ,"Offense" ,"Sentence Date" ,"Offense Date" ,"Sentence Years" ,"Last Parole Decision" ,"Next Parole Review Date" ,"Parole Review Status") values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )', x)
+    count += 1
+    print count
+conn.commit()
